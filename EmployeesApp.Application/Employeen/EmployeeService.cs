a264209bc17ad5ef133ed0d23d21ad45252f947a
Iryna <iryna.chorna@hotmail.com>
2025-05-26 14:25:27 +0200

    Added Logic to Terminal app

--- EmployeesApp.Application/Employeen/EmployeeService.cs
+++ EmployeesApp.Application/Employeen/EmployeeService.cs
@@ -9,7 +9,19 @@ public class EmployeeService(IEmployeeRepository employeeRepository) : IEmployee
 
     public Employee[] GetAll() => [.. employeeRepository.GetAll()];
 
-    public Employee GetById(int id) => employeeRepository.GetById(id);
+    public Employee GetById(int id)
+    {
+        try
+        {
+            var employee = employeeRepository.GetById(id);
+            return employee;
+        }
+        catch (Exception ex)
+        {
+
+            throw new ArgumentException($"Employee with id {id} not found. {ex.Message}");
+        }
+    }
 
     public bool CheckIsVIP(Employee employee) =>
         employee.Email.StartsWith("ADMIN", StringComparison.CurrentCultureIgnoreCase);
--- EmployeesApp.Terminal/Program.cs
+++ EmployeesApp.Terminal/Program.cs
@@ -6,10 +6,31 @@ internal class Program
 {
     static void Main(string[] args)
     {
+
         var employees = new EmployeeService(new EmployeeRepository()).GetAll();
         foreach (var employee in employees)
         {
             Console.WriteLine($"{employee.Name} - {employee.Email}");
         }
+
+        Console.Write("Enter a valid number: ");
+        try
+        {
+            if (int.TryParse(Console.ReadLine(), out int id) != null)
+            {
+                var emp = new EmployeeService(new EmployeeRepository()).GetById(id);
+                Console.WriteLine($"Employee Name: {emp.Name}");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid number entered.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
     }
 }
